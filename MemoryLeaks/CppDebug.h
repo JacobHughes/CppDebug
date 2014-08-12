2a25731d42aebcd20b7e17305489833e235e693b
JacobHughes <yacolt@gmail.com>
2014-08-12 14:57:15 +1000

    Memory leak debugging functions
    This folder contains functions and headers for debugging memory leaks

---
+++ MemoryLeaks/CppDebug.h
@@ -0,0 +1,28 @@
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+//=================
+// DEBUG TIME!
+//=================
+#ifdef _DEBUG
+#ifndef DBG_NEW
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#define new DBG_NEW
+#endif
+#endif  // _DEBUG
+
+/*==================================================
+	Function  Prototypes 
+==================================================*/
+
+//Initial function to set up the debug environment
+int setUpDebugEnvironment();
+
+//Function to set a breakpoint on allocation of a specific memory location
+int debugMemoryLocation(int memoryLocation);

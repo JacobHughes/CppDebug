cdfb607357c259eaa506789d4fa1992dd81d7926
JacobHughes <yacolt@gmail.com>
2015-01-21 16:53:56 +1100

    More details
    Added a lot more things, makes key types work better, including strings

--- HashTable/HashBucket.h
+++ HashTable/HashBucket.h
@@ -1,33 +1,61 @@
 #pragma once
-#include "HashKeyFunctions.h"
 
 #include <vector>
 #include <string>
+#include <typeinfo>
 
 template <typename K, typename V>
 class HashBucket
 {
 public:
 	HashBucket(){};
-	HashBucket(K &key_, V &value_)
+	HashBucket(K key, V value)
 	{
-		this->key = key_;
-		this->value = value_;
+		this->key = key;
+		this->value = value;
 	}
-	~HashBucket(){};
+	~HashBucket(){}
 	
 	int getHash(int M)
 	{
-		V val = getKey();
+		K key = getKey();
 
-		return modifiedBernsteinHash(val, M);
-	};
+		int hashedIndex = modifiedBernsteinHash((void*)&key);
 
-	void setKey(K &key){ this->key = key; };
-	void setValue(V &value){ this->value = value; };
+		return hashedIndex % M;
+	}
+
+	inline unsigned modifiedBernsteinHash( void* v_key)
+	{
+		unsigned h = 0;
+		if (typeid(K) == typeid(string))
+		{
+			string * key_ = static_cast<string *>(v_key);
+			unsigned char *p = (unsigned char *)key_->c_str();
+			int len = sizeof(p) / sizeof(p[0]);
+			for (int i = 0; i < len; ++i)
+			{
+				h = 33 * h ^ p[i];
+			}
+		}
+		else
+		{
+			unsigned char *p = static_cast<unsigned char *>(v_key);
+			int len = sizeof(p) / sizeof(p[0]);
+			for (int i = 0; i < len; ++i)
+			{
+				h = 33 * h ^ p[i];
+			}
+		}
+
+		return h;
+	}
+
+	void setKey(K key){ this->key = key; }
+	void setValue(V value){ this->value = value; }
 	
-	const K getKey(){ return this->key; };
-	const V getValue(){ return this->value; };
+	K getKey(){ return this->key; };
+	V getValue(){ return this->value; };
 
 protected:
 	K key;
--- HashTable/HashTable.h
+++ HashTable/HashTable.h
@@ -5,6 +5,9 @@
 #include <vector>
 #include <string>
 #include <memory>
+#include <ostream>
+#include <iostream>
+#include <typeinfo>
 
 using namespace std;
 
@@ -14,21 +17,42 @@ class HashTable
 public:
 	HashTable(int tableSize)
 	{
-		this->table.resize(tableSize);
+		this->tableSize = tableSize;
+		this->table.resize(this->tableSize);
 	}
 
-	~HashTable(){};
+	~HashTable(){}
 
-	HashBucket<K, V>* find(K &key)
+	HashBucket<K, V>* find(K key)
 	{
 		HashBucket<K, V> tempBucket;
 		tempBucket.setKey(key);
-		int index = tempBucket.getHash(table.size());
+		
+		int index = tempBucket.getHash(this->tableSize);
+
 		for (int i = 0; i < table[index].size(); ++i)
 		{
-			if (table[index][i].getKey() == key)
+			if (this->table[index][i].getKey() == key)
 			{
-				return &table[index][i];
+				return &this->table[index][i];
+			}
+		}
+
+		return NULL;
+	}
+
+	HashBucket<K, V>* find(K key, V value)
+	{
+		HashBucket<K, V> tempBucket;
+		tempBucket.setKey(key);
+
+		int index = tempBucket.getHash(this->tableSize);
+
+		for (int i = 0; i < this->table[index].size(); ++i)
+		{
+			if (this->table[index][i].getKey() == key && this->table[index][i].getValue() == value)
+			{
+				return &this->table[index][i];
 			}
 		}
 
@@ -36,17 +60,67 @@ public:
 	}
 
 	void erase(HashBucket<K, V> * pos)
-	{}
-
-	void insert(K &key, V &value)
 	{
-		HashBucket<K, V> bucket(key, value);
+		if (pos == NULL) { return; }
 
-		int index = bucket.getHash(this->table.size());
-		table[index].push_back(bucket);
+		int index = pos->getHash(this->tableSize);
+
+		int i = 0;
+
+		while (&this->table[index][i] != pos && i < this->table[index].size())
+		{
+			++i;
+		}
+
+		for (int j = i; j < this->table[index].size() - 1; ++j)
+		{
+			this->table[index][j] = this->table[index][j + 1];
+		}
+
+		this->table[index].pop_back();
 	}
 
-private:
-	std::vector<std::vector<HashBucket<K, V>>>table;
+	void insert(K key, V value)
+	{
+		HashBucket<K, V> bucket(key, value);
+		
+		int index = bucket.getHash(this->tableSize);
+
+		if (this->table[index].size() > 0)
+		{
+			++collisionCounter;
+		}
+
+		++entryCounter;
+		this->table[index].push_back(bucket);
+
+	}
+
+	int getCollisions(){ return this->collisionCounter; };
+
+	friend ostream & operator<< (ostream& out, const HashTable<K, V>& right)
+	{
+		for (int i = 0; i < right.table.size(); ++i)
+		{
+			out << i << ": ";
+			for (int j = 0; j < right.table[i].size(); ++j)
+			{
+				out << "x ";
+			}
+			out << "\n";
+		}
+		return out;
+	}
+
+	double getLoadFactor()
+	{
+		return static_cast<double>(entryCounter) / tableSize;
+	}
+
+protected:
+	std::vector<std::vector<HashBucket<K, V>>> table;
+	int tableSize;
+	int collisionCounter = 0;
+	int entryCounter = 0;
 };
 

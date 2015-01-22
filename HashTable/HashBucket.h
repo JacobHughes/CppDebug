dee36feb59b1aa540246d7c70edf7a8605bf6e8a
JacobHughes <yacolt@gmail.com>
2015-01-22 13:54:37 +1100

    Added comments
    Added lots of comments, including header

--- HashTable/HashBucket.h
+++ HashTable/HashBucket.h
@@ -1,3 +1,14 @@
+/*=======================================================
+	Code written by Jacob Hughes
+
+	Copyright (C) Jacob Hughes 2015
+
+	Contact for use:
+
+	yacolt@gmail.com
+	https://github.com/JacobHughes/
+=======================================================*/
+
 #pragma once
 
 #include <vector>
@@ -7,57 +18,101 @@
 template <typename K, typename V>
 class HashBucket
 {
+	/*
+		This class works with a HashTable.
+
+		Each HashBucket object holds an object of type V 
+		(determined with HashTable<K,V> and generates a
+		hash key for it, to determine where to insert in 
+		the HashTable.
+
+		The key generator implementation is currently the
+		modified Bernstein Hash function.
+	*/
 public:
-	HashBucket(){};
+	//Default constructor
+	HashBucket(){}
+
+	//Specific constructor
 	HashBucket(K key, V value)
 	{
 		this->key = key;
 		this->value = value;
 	}
+
+	//Destructor
 	~HashBucket(){}
 	
-	int getHash(int M)
+	//Get the hashed index for table insertion
+	int getHash(int tableSize)
 	{
+		//Get the key
 		K key = getKey();
 
+		//Get the general index
 		int hashedIndex = modifiedBernsteinHash((void*)&key);
 
-		return hashedIndex % M;
+		//Mod by table size and return
+		return hashedIndex % tableSize;
 	}
 
+	//Function to generate a hash
 	inline unsigned modifiedBernsteinHash( void* v_key)
 	{
+		//return variable for the hash index
 		unsigned h = 0;
+
+		//If statement to determine the typeid of the key K
+		//if it is a string, we need to generate the key
+		//slightly differently
 		if (typeid(K) == typeid(string))
 		{
+			//If K is a string:
+			//Cast into a string
 			string * key_ = static_cast<string *>(v_key);
+			//cast to an unsigned char *
 			unsigned char *p = (unsigned char *)key_->c_str();
+			//get the length (number of characters in p)
 			int len = sizeof(p) / sizeof(p[0]);
+			//For each character, do the hash function
 			for (int i = 0; i < len; ++i)
 			{
+				//Hash function
 				h = 33 * h ^ p[i];
 			}
 		}
 		else
 		{
+			//If K is anything else:
+			//Cast into an unsigned char *
 			unsigned char *p = static_cast<unsigned char *>(v_key);
+			//Get length (number of characters in p)
 			int len = sizeof(p) / sizeof(p[0]);
+			//For each character, do the hash function
 			for (int i = 0; i < len; ++i)
 			{
+				//Hash function
 				h = 33 * h ^ p[i];
 			}
 		}
 
+		//return the result of the hash function
 		return h;
 	}
 
+	//Set the key
 	void setKey(K key){ this->key = key; }
+	//Set the data held by the bucket
 	void setValue(V value){ this->value = value; }
 	
+	//Get the key
 	K getKey(){ return this->key; };
+	//Get the data held by the bucket
 	V getValue(){ return this->value; };
 
 protected:
+	//Key
 	K key;
+	//Data held by bucket
 	V value;
 };
--- HashTable/HashTable.h
+++ HashTable/HashTable.h
@@ -1,3 +1,15 @@
+/*=======================================================
+	Code written by Jacob Hughes
+
+	Copyright (C) Jacob Hughes 2015
+
+	Contact for use:
+
+	yacolt@gmail.com
+	https://github.com/JacobHughes/
+=======================================================*/
+
+
 #pragma once
 
 #include "HashBucket.h"
@@ -14,91 +26,145 @@ using namespace std;
 template <typename K, typename V>
 class HashTable
 {
+	/*
+		This is my implementation of a generic template class for
+		a hash table. Type K is the key-type, and V is the data.
+		This allows any object as the Key or Value.
+
+		This implemenation uses a 2D-array for the hash table, 
+		implented with vectors of HashBucket objects.
+
+		The HashBucket objects hold and generate hash keys, as well
+		as holding the data.
+
+		This implementation will count the number of collisions, 
+		and report the load factor of the HashTable.
+	
+	*/
 public:
+	//Constructor - set the size of the table
 	HashTable(int tableSize)
 	{
 		this->tableSize = tableSize;
 		this->table.resize(this->tableSize);
 	}
-
+	
+	//Destructor - default
 	~HashTable(){}
 
+	//Function to find data based on the key
 	HashBucket<K, V>* find(K key)
 	{
+		//Get a temporary bucket to generate a key
 		HashBucket<K, V> tempBucket;
 		tempBucket.setKey(key);
 		
+		//Find the index of this key
 		int index = tempBucket.getHash(this->tableSize);
 
+		//Loop through all the data stored in the index
 		for (int i = 0; i < table[index].size(); ++i)
 		{
+			//If the key matches..
 			if (this->table[index][i].getKey() == key)
 			{
+				//...return the HashBucket object
 				return &this->table[index][i];
 			}
 		}
 
+		//Return NULL since nothing matched our search
 		return NULL;
 	}
 
+
+	//Function to find data with the key and value known
 	HashBucket<K, V>* find(K key, V value)
 	{
+		//Get a temporary bucket to generate a key
 		HashBucket<K, V> tempBucket;
 		tempBucket.setKey(key);
 
+		//Find the index of this key
 		int index = tempBucket.getHash(this->tableSize);
 
+		//Loop through all the data stored in the index
 		for (int i = 0; i < this->table[index].size(); ++i)
 		{
+			//if the key AND value matches...
 			if (this->table[index][i].getKey() == key && this->table[index][i].getValue() == value)
 			{
+				//...return the HashBucket object
 				return &this->table[index][i];
 			}
 		}
 
+		//Return NULL since nothing matched our search
 		return NULL;
 	}
 
+	//Function to erase a bucket
 	void erase(HashBucket<K, V> * pos)
 	{
+		//If we are handed a NULL pointer, invalid
 		if (pos == NULL) { return; }
 
+		//Find the index of the bucket
 		int index = pos->getHash(this->tableSize);
 
+		//Loop variable
 		int i = 0;
 
+		//Loop around to find the index of our bucket
 		while (&this->table[index][i] != pos && i < this->table[index].size())
 		{
 			++i;
 		}
 
+		//Move any other objects with the same index forwards
 		for (int j = i; j < this->table[index].size() - 1; ++j)
 		{
 			this->table[index][j] = this->table[index][j + 1];
 		}
 
+		//Then pop-back to delete our specific bucket
 		this->table[index].pop_back();
+
+		//Decrement the number of entries
+		--entryCounter;
 	}
 
+	//Function to insert to the table
 	void insert(K key, V value)
 	{
+		//Create a new bucket to hold the value
 		HashBucket<K, V> bucket(key, value);
 		
+		//Find the insertion index
 		int index = bucket.getHash(this->tableSize);
 
+		//If this index results in a collision...
 		if (this->table[index].size() > 0)
 		{
+			//...increment the collision counter
 			++collisionCounter;
 		}
 
-		++entryCounter;
+		//Add the bucket to the table
 		this->table[index].push_back(bucket);
+
+		//Increment the number of objects in the table
+		++entryCounter;
 
 	}
 
+	//Function to return the number of collision from insertion
 	int getCollisions(){ return this->collisionCounter; };
 
-	friend ostream & operator<< (ostream& out, const HashTable<K, V>& right)
+	//Operator to output the Hash Table to a representation in text
+	//Each line has the bucket number, and an 'x' to represent the number of
+	//objects in each bucket.
+	friend ostream & operator<< (ostream & out, const HashTable<K, V> & right)
 	{
 		for (int i = 0; i < right.table.size(); ++i)
 		{
@@ -112,15 +178,22 @@ public:
 		return out;
 	}
 
+	//Function to return the load factor	
 	double getLoadFactor()
 	{
+		//Return the load factor (defined as number of objects
+		//in the table / number of buckets
 		return static_cast<double>(entryCounter) / tableSize;
 	}
 
 protected:
+	//The hash table, in vectors
 	std::vector<std::vector<HashBucket<K, V>>> table;
+	//Size of the table
 	int tableSize;
+	//Number of collisions
 	int collisionCounter = 0;
+	//Number of objects in the table
 	int entryCounter = 0;
 };
 

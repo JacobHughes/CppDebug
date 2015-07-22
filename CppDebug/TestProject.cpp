394373b1e98cdbd302c5562889ceacc5d19c4669
JacobHughes <yacolt@gmail.com>
2015-07-22 16:08:36 +1000

    Added selection sort and testing

--- CppDebug/TestProject.cpp
+++ CppDebug/TestProject.cpp
@@ -4,23 +4,64 @@
 
 using namespace std;
 
-int main()
+void testSelectionSort(void)
 {
-	Logger::instance().log(LoggerLevel_critical, "Does this project work?");
-
+	Logger::instance().log(LoggerLevel_normal, "Now testing selection sort");
 	vector<int> myVector;
+	myVector.push_back(10);
+	myVector.push_back(9);
+	myVector.push_back(8);
+	myVector.push_back(7);
+	myVector.push_back(6);
 	myVector.push_back(5);
 	myVector.push_back(4);
 	myVector.push_back(3);
 	myVector.push_back(2);
 	myVector.push_back(1);
 
+	string myVectorStr;
+	for (auto i = 0; i < myVector.size(); ++i)
+	{
+		myVectorStr += to_string(myVector.at(i));
+		myVectorStr += " ";
+	}
+
+	vector<int> myVectorOriginal = myVector;
+	vector<int> myVector2 = myVector;
+
+	Logger::instance().log(LoggerLevel_normal, "Before sorting myVector is: " + myVectorStr);
+
 	basicSelectionSort(myVector);
 
-	for (int i = 0; i < myVector.size(); ++i)
+	myVectorStr = "";
+	for (auto i = 0; i < myVector.size(); ++i)
 	{
-		Logger::instance() << to_string(myVector.at(i));
+		myVectorStr += to_string(myVector.at(i));
+		myVectorStr += " ";
 	}
+
+	Logger::instance().log(LoggerLevel_normal, "After basicSelectionSort myVector is: " + myVectorStr);
+
+	vector<int>::iterator f = myVector2.begin();
+	vector<int>::iterator l = myVector2.end();
+
+	selection_sort(f, l);
+
+	myVectorStr = "";
+	for (auto i = 0; i < myVector2.size(); ++i)
+	{
+		myVectorStr += to_string(myVector2.at(i));
+		myVectorStr += " ";
+	}
+
+	Logger::instance().log(LoggerLevel_normal, "After selection_sort myVector is: " + myVectorStr);
+}
+
+int main()
+{
+	Logger::instance().log(LoggerLevel_critical, "Does this project work? Let's see...");
+
+	testSelectionSort();
 
 	Logger::instance() << "Press the enter key to exit...";
 	std::cin.ignore(std::cin.rdbuf()->in_avail() + 1);
--- Sorting/sorting.h
+++ Sorting/sorting.h
@@ -7,6 +7,20 @@ using namespace std;
 template<typename T>
 static void basicSelectionSort(vector<T>& t)
 {
+	/*
+		This is a basic (perhaps naive) implementation of a 
+		selection sort algorithm, in it's most basic form.
+
+		We evaluate run through each item in the vector, and 
+		swap it with the largest value in the rest of the 
+		vector.
+
+		Complexity/efficiency:
+		Best case:		O( n^2 )
+		Worst case:		O( n^2 )
+		Average case:	O( n^2 )
+	
+	*/
 	for (int last = (t.size()-1); last >= 1; last--)
 	{
 		int largest = 0;
@@ -19,8 +33,35 @@ static void basicSelectionSort(vector<T>& t)
 			}
 		}
 
+		//Perform the swap here
 		T temp = t.at(largest);
 		t.at(largest) = t.at(last);
 		t.at(last) = temp;
+	}
+}
+
+template< typename ForwardIterator, typename Compare = std::less<typename std::iterator_traits<ForwardIterator>::value_type> >
+void selection_sort(ForwardIterator first, ForwardIterator last, Compare cmp = Compare())
+{
+	/*
+		This is more specialised C++ implementation of the selection sort
+		which makes use of iterators and std operations, which should be 
+		more optimised.
+
+		This implementation allows user specified comparison function, 
+		so non-default or primitive objects can be sorted as required.
+		The default comparison is operator<
+
+		Complexity/efficiency:
+		Best case:		O( n^2 )
+		Worst case:		O( n^2 )
+		Average case:	O( n^2 )
+	*/
+
+	for (auto i = first; i != last; ++i) {
+		//Find the smallest element
+		auto const selection = std::min_element(i, last, cmp);
+		//Swap elements
+		std::iter_swap(selection, i);
 	}
 }

ef4806c1632e54694da8a8d8a6b403c0bcc9eb96
JacobHughes <yacolt@gmail.com>
2016-04-23 18:51:25 +1000

    Added 2D vector too

--- VectorMath/VectorMath.h
+++ VectorMath/VectorMath.h
@@ -7,10 +7,12 @@
 
 #pragma once
 
-//#ifndef _USE_MATH_DEFINES
+#ifndef M_PI
 #define M_PI 3.14159265358979323846
+#endif // !M_PI
+
 #include <cmath>
-//#endif // !_USE_MATH_DEFINES
+
 
 
 
@@ -60,17 +62,20 @@ namespace VectorMath
 	{
 		lhs.x += rhs.x;
 		lhs.y += rhs.y;
-		lhs.z += rhs.y;
+		lhs.z += rhs.z;
 		return lhs;
 	}
 
-	const Vector3D operator+(const Vector3D& lhs, const Vector3D& rhs) { return Vector3D (lhs) += rhs;}
+	const Vector3D operator+(const Vector3D& lhs, const Vector3D& rhs) 
+	{
+		return Vector3D (lhs) += rhs;
+	}
 
 	const Vector3D operator-=(Vector3D& lhs, const Vector3D& rhs)
 	{
 		lhs.x -= rhs.x;
 		lhs.y -= rhs.y;
-		lhs.z -= rhs.y;
+		lhs.z -= rhs.z;
 		return lhs;
 	}
 
@@ -127,4 +132,79 @@ namespace VectorMath
 	}
 
 	
+	class Vector2D
+	{
+		/*=======================================================
+			This class is used to represent a 2 dimensional
+			vector, with associated useful member methods,
+			operators
+		=======================================================*/
+	public:
+
+		Vector2D() : x(0.0), y(0.0) {}
+		Vector2D(double x_, double y_) : x(x_), y(y_) {}
+		//~Vector2D();
+
+		//Component vector values
+		double x, y;
+
+		//Initialise with another Vector3D
+		Vector2D(const Vector2D& vec) : x(vec.x), y(vec.y) {}
+
+		//Get the vector length
+		double getLength() { return std::sqrt(x*x + y*y); }
+	};
+
+	const bool operator== (const Vector2D& lhs, const Vector2D& rhs)
+	{
+		return lhs.x == rhs.x && lhs.y == rhs.y;
+	}
+
+	const bool operator!= (const Vector2D& lhs, const Vector2D& rhs)
+	{
+		return !operator==(lhs, rhs);
+	}
+
+	//Multiplication by a scalar
+	const Vector2D operator*(const double & scalar, Vector2D vec)
+	{
+		return Vector2D(scalar*vec.x, scalar*vec.y);
+	}
+
+	const Vector2D operator+=(Vector2D& lhs, const Vector2D& rhs)
+	{
+		lhs.x += rhs.x;
+		lhs.y += rhs.y;
+		return lhs;
+	}
+
+	const Vector2D operator+(const Vector2D& lhs, const Vector2D& rhs)
+	{
+		return Vector2D(lhs) += rhs;
+	}
+
+	const Vector2D operator-=(Vector2D& lhs, const Vector2D& rhs)
+	{
+		lhs.x -= rhs.x;
+		lhs.y -= rhs.y;
+		return lhs;
+	}
+
+	const Vector2D operator-(const Vector2D& lhs, const Vector2D& rhs)
+	{
+		return Vector2D(lhs) -= rhs;
+	}
+
+	//Dot product of Lhs and rhs: LHS DOT RHS
+	const double dotProduct(const Vector2D& lhs, const Vector2D &rhs)
+	{
+		return ((lhs.x * rhs.x) + (lhs.y * rhs.y));
+	}
+
+	//Get the normalised Vector2D
+	const Vector2D normaliseVector2D(Vector2D& vec)
+	{
+		return Vector2D(vec.x / vec.getLength(), vec.y / vec.getLength());
+	}
+
 }
